d3e40bcaf04b8dd525dd4e78079b82007ef3aeec
ililcheva <ivailcheva@mail.bg>
2017-07-13 18:09:44 +0300

    new cool features added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,20 +4,20 @@ import { RouterModule, Routes }     from '@angular/router';
 
 import { HomeComponent }        from '../home/home.component';
 import { BookListComponent }        from '../book-list/book-list.component';
-//import { BookDetailComponent }      from '../book-detail/book-detail.component';
+import { BookDetailComponent }      from '../book-detail/book-detail.component';
 import { PageNotFoundComponent }    from '../page-not-found/page-not-found.component';
 
 const routes: Routes = [
     { path: '',            redirectTo: '/home', pathMatch: 'full'}, //default path
     { path: 'home',       component: HomeComponent},
-//    { path: 'books/:id',   component: BookDetailComponent }, // not sure if going to implement this one
     { path: 'books',       component: BookListComponent }, //books-list path
+    { path: 'books/:id',   component: BookDetailComponent }, // book-details path
     { path: '**',          component: PageNotFoundComponent} //page-not-found path
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {enableTracing: true})
+    RouterModule.forRoot(routes)
   ],
   exports: [ RouterModule ]
 })
--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input }       from '@angular/core';
+import { ActivatedRoute, ParamMap }       from '@angular/router';
+import { Location }                       from '@angular/common';
+
+//switchMap for processing the route parameters from an observable<book> to a book
+import 'rxjs/add/operator/switchMap';
+
+import { Book }                           from '../book';
+import { BookService }                    from '../book.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -6,10 +14,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-detail.component.css']
 })
 export class BookDetailComponent implements OnInit {
+  @Input() book: Book;
+  private currentBook: Book;
 
-  constructor() { }
+  //injecting services that this component requires
+  constructor(
+    private bookService: BookService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    //getting the id parameter and processing it
+    this.route.paramMap
+        .switchMap((parameters: ParamMap) => this.bookService.getBook(parseInt(parameters.get('id'),10)))
+        //subscribe() wants the book and not the observable
+        .subscribe((book) => this.book = book);
   }
+
+  goBack(): void{
+    //navigates back to book's list
+    this.location.back();
+  }
+
+  save(id: number): void{
+    //save changes
+    console.log('*Save method:* //book-detail.component');
+    this.bookService.update(id, this.book)
+        .then(() => this.goBack());
+  }
+
+
 
 }
