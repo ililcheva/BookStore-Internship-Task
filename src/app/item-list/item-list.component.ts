d9c91227fa20bbbc366bf6f78389d69b429c714c
ililcheva <ivailcheva@mail.bg>
2017-07-23 17:48:14 +0300

    added book filtration by store

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { Component, OnInit, Input, Output,EventEmitter }                         from '@angular/core';
+=======
+import { Component, OnInit, Input }                         from '@angular/core';
+>>>>>>> added book filtration by store
 import { Router}            from '@angular/router';
 
 import { Book }              from '../book';
