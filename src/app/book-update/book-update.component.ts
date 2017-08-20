c89e4e7ee393cffd99f2425753761bd15ba30019
ililcheva <ivailcheva@mail.bg>
2017-08-20 19:54:14 +0300

    Deleted some components that are useless now

--- src/app/book-update/book-update.component.ts
+++ src/app/book-update/book-update.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Input }                    from '@angular/core';
-import { ActivatedRoute, ParamMap, Router }            from '@angular/router';
-import { Location }                                    from '@angular/common';
-import { FormBuilder, FormGroup, Validators }          from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import 'rxjs/add/operator/switchMap';
 
 
-import { Book }                                        from '../book';
-import { BookService }                                 from '../book.service';
+import { Book } from '../book';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-update',
@@ -23,43 +23,43 @@ export class BookUpdateComponent implements OnInit {
     private location: Location,
     private route: ActivatedRoute,
     private fb: FormBuilder
-  ) { 
+  ) {
 
     this.bookForm = this.fb.group({
-      name: ['',  Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z\s\'\".-]*$/)])],
+      name: ['', Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z\s\'\".-]*$/)])],
       author: ['', Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z\s.-]*$/)])],
-      description: ['', Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500) ])],
+      description: ['', Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
       price: ['', Validators.compose([Validators.required, Validators.pattern(/^[0-9]*$/)])],
       rating: ['', Validators.compose([Validators.pattern(/^[1-5]{1}$/)])],
-      pictureUrl: ['', ],
+      pictureUrl: ['',],
     })
   }
 
   ngOnInit() {
     //getting the id parameter and processing it
     this.route.paramMap
-        .switchMap((parameters: ParamMap) => this.bookService.getBook(parseInt(parameters.get('id'),10)))
-        //subscribe() wants the book and not the observable
-        .subscribe((book) => this.book = book);
+      .switchMap((parameters: ParamMap) => this.bookService.getBook(parseInt(parameters.get('id'), 10)))
+      //subscribe() wants the book and not the observable
+      .subscribe((book) => this.book = book);
   }
 
-  goBack(): void{
+  goBack(): void {
     //navigates back to book's list
     this.location.back();
   }
 
-  save(id: number): void{
+  save(id: number): void {
     //save changes
     console.log('*Save method:* //book-detail.component');
     this.bookService.update(id, this.book)
-        .then(() => this.goBack());
+      .then(() => this.goBack());
   }
 
-  create(book): void{
+  create(book): void {
     this.bookService.create(this.book)
-        .then(() => {
-          console.log('Book =', book);
-          this.goBack();
-        });
+      .then(() => {
+        console.log('Book =', book);
+        this.goBack();
+      });
   }
 }
