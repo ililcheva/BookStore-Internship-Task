7e2877268f296f7fb5ed0034664c4c0299dd3454
ililcheva <ivailcheva@mail.bg>
2017-08-20 19:54:14 +0300

    Deleted some components that are useless now

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,30 +1,30 @@
-import { NgModule }                 from '@angular/core';
-import { CommonModule }             from '@angular/common';
-import { RouterModule, Routes }     from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent }                  from '../home/home.component';
-import { BookDetailComponent }            from '../book-detail/book-detail.component';
-import { BookUpdateComponent }            from '../book-update/book-update.component';
-import { PageNotFoundComponent }          from '../page-not-found/page-not-found.component';
-import { BooksComponent }               from '../books/books.component';
+import { HomeComponent } from '../home/home.component';
+import { BookDetailComponent } from '../book-detail/book-detail.component';
+import { BookUpdateComponent } from '../book-update/book-update.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { BooksComponent } from '../books/books.component';
 
 const routes: Routes = [
-    { path: '',                   redirectTo: '/home', pathMatch: 'full'}, //default path
-    { path: 'home',               component: HomeComponent},
-    { path: 'books',              component: BooksComponent}, //books-list path
-    { path: 'book/:id',           component: BookDetailComponent }, // book-details path
-    { path: 'book/update/:id',    component: BookUpdateComponent },
-    { path: 'stores',             component: BooksComponent },
-    { path: 'store/:id',          component: BooksComponent },
-    { path: 'store/:id/book/:id', component: BookDetailComponent },
-    { path: 'items',              component: BooksComponent },
-    { path: '**',                 component: PageNotFoundComponent} //page-not-found path
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, //default path
+  { path: 'home', component: HomeComponent },
+  { path: 'books', component: BooksComponent }, //books-list path
+  { path: 'book/:id', component: BookDetailComponent }, // book-details path
+  { path: 'book/update/:id', component: BookUpdateComponent },
+  { path: 'stores', component: BooksComponent },
+  { path: 'store/:id', component: BooksComponent },
+  { path: 'store/:id/book/:id', component: BookDetailComponent },
+  { path: 'items', component: BooksComponent },
+  { path: '**', component: PageNotFoundComponent } //page-not-found path
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports: [ RouterModule ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
